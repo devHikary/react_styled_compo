0706448eee96d4d020bb08adb5147867a5df7257
devHikary <wrkkarina@gmail.com>
2021-09-25 05:47:50 -0300

    GlobalStyle

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import { GlobalStyle } from './Components/Globalstyle'
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
     </>
