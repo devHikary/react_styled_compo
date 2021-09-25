e5bc1c84cd7394cf170c88d10d807d1a289b7436
devHikary <wrkkarina@gmail.com>
2021-09-25 16:32:55 -0300

    config tema

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { ThemeProvider } from 'styled-components';
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from './Components/Globalstyle'
+import { temaClaro, temaEscuro } from './Components/UI/temas';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Cabecalho />
-      <Container />
+      <ThemeProvider theme={temaClaro}>
+        <GlobalStyle />
+        <Cabecalho />
+        <Container />
+      </ThemeProvider>
     </>
   );
 }
